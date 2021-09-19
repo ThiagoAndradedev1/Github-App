b31fe9de911fec11228107ec92387b0bb3b93d5f
Thiago <thiago.andrade.dev1@gmail.com>
2021-09-19 15:48:23 -0300

    Criação do componente About

---
+++ src/components/pages/About.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div className="card grid-2">
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        Hello
+      </div>
+    </div>
+  );
+};
+
+export default About;
