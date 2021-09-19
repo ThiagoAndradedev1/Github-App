91cf5bd1b521c164317758b88af5b0138e4f507f
Thiago <thiago.andrade.dev1@gmail.com>
2021-09-19 15:41:32 -0300

    Adicionando alert context

--- src/context/types.js
+++ src/context/types.js
@@ -3,3 +3,6 @@ export const SEARCH_REPOS = "SEARCH_REPOS";
 export const SEARCH_STARRED = "SEARCH_STARRED";
 export const CLEAR_USERS = "CLEAR-USERS";
 export const SET_LOADING = "SET_LOADING";
+export const SET_ALERT = "SET_ALERT";
+export const REMOVE_ALERT = "REMOVE_ALERT";
+export const HANDLE_ERROR = "HANDLE_ERROR";
