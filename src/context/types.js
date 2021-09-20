832cfae54d51f2104197dfe8f7be6a33aec96475
Thiago <thiago.andrade.dev1@gmail.com>
2021-09-20 12:20:50 -0300

    Modificações no arquivo types

--- src/context/types.js
+++ src/context/types.js
@@ -1,7 +1,7 @@
 export const SEARCH_USER = "SEARCH_USER";
 export const SEARCH_REPOS = "SEARCH_REPOS";
 export const SEARCH_STARRED = "SEARCH_STARRED";
-export const CLEAR_USERS = "CLEAR-USERS";
+export const CLEAR_USERS = "CLEAR_USERS";
 export const SET_LOADING = "SET_LOADING";
 export const SET_ALERT = "SET_ALERT";
 export const REMOVE_ALERT = "REMOVE_ALERT";
